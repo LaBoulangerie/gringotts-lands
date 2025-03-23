bd840c99afdb215a2b3800c6de64b84c9576ca88
Clem <clemfern456@gmail.com>
2025-03-23 10:56:53 +0100

    import never used

--- src/main/java/net/laboulangerie/gringottslands/GringottsLands.java
+++ src/main/java/net/laboulangerie/gringottslands/GringottsLands.java
@@ -12,7 +12,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class GringottsLands extends JavaPlugin {
